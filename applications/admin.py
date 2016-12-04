74bbfa081b7223ce302ae170524cd84fc9017ef7
caleb-st <caleb_stdenis@hotmail.com>
2016-12-04 17:15:50 -0500

    Customizing admin. It's a start

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User, Group, Permission
+from applications.models import Requester, Supervisor
+
+class RequesterAdmin(admin.ModelAdmin):
+    model = Requester
+
+class SupervisorAdmin(admin.ModelAdmin):
+    model = Supervisor
+
+admin.site.unregister(Group)
+admin.site.register(Requester, RequesterAdmin)
+admin.site.register(Supervisor, SupervisorAdmin)
+admin.site.site_header = 'CTGS Administration'
