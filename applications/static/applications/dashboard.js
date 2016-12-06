ae9a0384cfe8cf397ca6a0996b7faba21940d74d
caleb-st <caleb_stdenis@hotmail.com>
2016-12-05 23:16:57 -0500

    Make recommendation done

--- applications/static/applications/dashboard.js
+++ applications/static/applications/dashboard.js
@@ -4,5 +4,11 @@ $(".pendingrecommendation-table > tbody > tr").click(function() {
     window.location.href = $(this).attr('href');
 });
 
+ $("#request-changes").on("hide.bs.collapse", function(){
+    $(".btn-request-changes").html('<span class="glyphicon glyphicon-collapse-down"></span> Request Changes');
+  });
+  $("#request-changes").on("show.bs.collapse", function(){
+    $(".btn-request-changes").html('<span class="glyphicon glyphicon-collapse-up"></span> Request Changes');
+  });
 });
 
